bd1b9251398bb9f085a15029333b63263a48ed29
mg.apisara <apisara.chon@mail.kmutt.ac.th>
2025-03-05 21:13:23 +0700

    set up pj

---
+++ FE/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+      },
+    },
+  },
+  plugins: [],
+}
+
