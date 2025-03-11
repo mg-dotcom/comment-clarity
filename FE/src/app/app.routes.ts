38452b52ed0c1efdb8e3a0c6d1f8f75eec0f3f97
mg.apisara <apisara.chon@mail.kmutt.ac.th>
2025-03-11 22:02:29 +0700

    create home page

--- FE/src/app/app.routes.ts
+++ FE/src/app/app.routes.ts
@@ -1,18 +1,26 @@
 import { Routes } from '@angular/router';
 
-
 export const routes: Routes = [
-    {
-        path: '',
-        redirectTo: 'login',
-        pathMatch: 'full'
-    },
-    {
-        path: 'login',
-        loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent)
-    },
-    {
-        path: 'register',
-        loadComponent: () => import('./pages/register/register.component').then(m => m.RegisterComponent)
-    },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./pages/login/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./pages/register/register.component').then(
+        (m) => m.RegisterComponent
+      ),
+  },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./pages/home/home.component').then((m) => m.HomeComponent),
+  },
 ];
--- FE/tailwind.config.js
+++ FE/tailwind.config.js
@@ -1,24 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {
       screens: {
-        'sm': '640px',
-        'md': '768px',
-        'lg': '1024px',
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
       },
       colors: {
-        'primary': '#FEF7FF',
-        'secondary': '#FFC0CB',
-        'button-primary': '#FF69B4',
-        'button-primary-darker': '#FF1493',
-        'button-primary-active': '#FF8DA1',
+        primary: "#FEF7FF",
+        secondary: "#FFC0CB",
+        "button-primary": "#FF69B4",
+        "button-primary-darker": "#FF1493",
+        "button-primary-active": "#FF8DA1",
+        "app-bg": "#fcf6ff",
+        "card-bg": "#f5eaf9",
+        "profile-bg": "#e8d5f5",
+        "add-button": "#ffd6e6",
+        "add-button-hover": "#ffc2d3",
       },
     },
   },
   plugins: [],
-}
-
+};
