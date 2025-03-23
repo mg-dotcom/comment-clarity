856ecc9ba50fc308654974f1e39db351090eef5d
mg.apisara <apisara.chon@mail.kmutt.ac.th>
2025-03-23 13:07:28 +0700

    update

--- FE/src/app/page/product/product-list/product-list.component.ts
+++ FE/src/app/page/product/product-list/product-list.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-list',
   imports: [],
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.css'
+  styleUrl: './product-list.component.css',
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+  constructor() {}
 
+  goToProductDetail(): void {
+    console.log('goToProductDetail');
+  }
+
+  ngOnInit() {
+    console.log('ProductListComponent');
+  }
 }
