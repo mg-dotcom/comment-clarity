3b4bc223defee9a6c42445f356308537acfb230d
mg.apisara <apisara.chon@mail.kmutt.ac.th>
2025-03-23 20:38:27 +0700

    font

--- FE/tailwind.config.js
+++ FE/tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
+    fontFamily: {
+      sans: ["Poppins", "Sarabun", "sans-serif"],
+    },
     extend: {
       screens: {
         sm: "640px",
